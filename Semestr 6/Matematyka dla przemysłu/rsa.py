815317e0a55762dfe845f0479dce434b97c8763e
Zuzanna_Sosnowska <126651779+prog-zuzanna-sosnowska@users.noreply.github.com>
2025-04-15 22:03:43 +0200

    KTLO

--- Semestr 6/Matematyka dla przemysłu/rsa.py
+++ Semestr 6/Matematyka dla przemysłu/rsa.py
@@ -1,17 +1,38 @@
-import numpy as np
 import sympy as sp
 import random
 
 
-def gcd( a, b):
+def extended_euclidean(a, b):
+    """
+    Zwraca (gcd, x, y) takie, że: a*x + b*y = gcd
+    """
     if b == 0:
-        return a
+        return a, 1, 0
     else:
-        return gcd(b, a % b)
+        gcd, x1, y1 = extended_euclidean(b, a % b)
+        x = y1
+        y = x1 - (a // b) * y1
+        return gcd, x, y
+
+
+def mod_inverse(e, phi):
+    """
+    Zwraca d takie, że (e * d) % phi == 1
+    """
+    gcd, x, _ = extended_euclidean(e, phi)
+    if gcd != 1:
+        raise Exception("Odwrotność nie istnieje")
+    else:
+        return x % phi
+
 
 def generate_rsa_keys(p: int, q: int):
-    n = p * q
-    totient = (p - 1) * (q - 1)
+
+    def gcd(a, b):
+        if b == 0:
+            return a
+        else:
+            return gcd(b, a % b)
 
     def choose_e(totient):
         while True:
@@ -19,9 +40,36 @@ def generate_rsa_keys(p: int, q: int):
             if gcd(e, totient) == 1:
                 return e
 
+    n = p * q
+    totient = (p - 1) * (q - 1)
     e = choose_e(totient)
+    d = mod_inverse(e, totient)
+    return n, e, d
 
 
+def encrypt(n, e, m):
+    return pow(m, e, n)
+
+
+def decrypt(n, d, c):
+    return pow(c, d, n)
 
 if __name__ == '__main__':
-    print(sp.randprime(10, 20))
+    p = sp.randprime(10 ** 40, 2 * 10 ** 40)
+    q = sp.randprime(10 ** 40, 2 * 10 ** 40)
+    n, e, d = generate_rsa_keys(p, q)
+    message = "Ala ma kota"
+    print("Nadana wiadomość:", message)
+
+    m = int(message.encode("utf-8").hex(), 16)
+
+    c = encrypt(n, e, m)
+    print("Szyfrogram w postaci liczby:", c)
+    m = decrypt(n, d, c)
+    print("Odszyfrowana wiadomość w postaci liczby:", m)
+
+    tmp = hex(m)[2:]
+    if len(tmp) % 2 == 1:
+        tmp = "0" + tmp
+    message = bytes.fromhex(tmp).decode("utf-8")
+    print("Odszyfrowana wiadomość:", message)
